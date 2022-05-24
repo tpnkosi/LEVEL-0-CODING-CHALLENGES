3b26382009018a5dd0b039892bc7513c3781696f
Thulile Nkosi <tp.nkosi28@gmail.com>
2022-05-24 14:25:51 +0200

    Completed Task 0.2

---
+++ Task2.js
@@ -0,0 +1,10 @@
+let x = 1 + 1 *2;
+let y = (1 + 1) *2;
+let z = 1 + (1 * 2);
+let a = 1 + 1 * 2 / 2;
+let b = (1 + 1 * 2) / 2;
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(a);
+console.log(b);
