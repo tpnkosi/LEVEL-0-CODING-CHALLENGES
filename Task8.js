413e2e93fd2b2f062f7676f591814a73e3078234
Thulile Nkosi <tp.nkosi28@gmail.com>
2022-05-25 13:52:26 +0200

    Accounted for plural and singular strings, and constant variables

--- Task8.js
+++ Task8.js
@@ -1,7 +1,31 @@
 function convertToHoursAndMinutes(minutes) {
-    let hour = Math.floor(minutes / 60);
-    let min = minutes % 60;
-    return (hour + " hours, " + min + " minutes.")
+    
+    const hour = Math.floor(minutes / 60);
+    let stringHour;
+    switch(hour) {
+        case 1:
+            stringHour = "hour";
+            break;
+        default:
+            stringHour = "hours";
+            break;
+    }
+
+    const min = minutes % 60;
+    let stringMinutes;
+    switch(minutes) {
+        case 1:
+            stringMinutes = "minute";
+            break;
+        default:
+            stringMinutes = "minutes";
+            break;
+    }
+
+    return `${hour} ${stringHour}, ${min} ${stringMinutes}`;
 }
 
 console.log(convertToHoursAndMinutes(71));
+console.log(convertToHoursAndMinutes(264));
+console.log(convertToHoursAndMinutes(241));
+console.log(convertToHoursAndMinutes(15));
