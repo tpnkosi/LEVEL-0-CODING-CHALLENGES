c1cddb62aaf8a0471550ef1903a5125bf9eee542
Thulile Nkosi <tp.nkosi28@gmail.com>
2022-05-25 13:31:26 +0200

    Declared constant variables as such

--- Task2.js
+++ Task2.js
@@ -1,8 +1,8 @@
-let x = 1 + 1 *2;
-let y = (1 + 1) *2;
-let z = 1 + (1 * 2);
-let a = 1 + 1 * 2 / 2;
-let b = (1 + 1 * 2) / 2;
+const x = 1 + 1 *2;
+const y = (1 + 1) *2;
+const z = 1 + (1 * 2);
+const a = 1 + 1 * 2 / 2;
+const b = (1 + 1 * 2) / 2;
 console.log(x);
 console.log(y);
 console.log(z);
