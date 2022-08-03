f328270c601d812174b2833c3ab2060f9a6aac51
Thulile Nkosi <tp.nkosi28@gmail.com>
2022-08-03 11:17:43 +0200

    Provided a more descriptive name for the semiperimeter

--- Task5.js
+++ Task5.js
@@ -1,7 +1,7 @@
 function triangleArea(side1, side2, side3) {
     //Herons formula for area: A = sqr s(s-a)(s-b)(s-c), where s = semiperimeter
-    const s = 1/2 * (side1+side2+side3);
-    const area = Math.round(Math.sqrt(s*(s-side1)*(s-side2)*(s-side3)));
+    const semiPerimeter = 1/2 * (side1+side2+side3);
+    const area = Math.round(Math.sqrt(semiPerimeter*(semiPerimeter-side1)*(semiPerimeter-side2)*(semiPerimeter-side3)));
     return area;
 }
 
